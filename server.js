4d14d33802d2a6731002f1d89601b8ffe1011745
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-05-31 19:42:00 -0700

    get/post

--- server.js
+++ server.js
@@ -8,3 +8,29 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
 const PORT = process.env.Port || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('./Develop/public'));
+
+app.get('api/notes', function(req, res) {
+    readFileAsync('./Develop/db/db.json', 'utf8').then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
+
+app.post('/api/notes', function(req, res) {
+    const note = req.body;
+    readFileAsync('./Develop/db/db.json', 'utf8').then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(notes);
+        return notes
+    }).then(function(notes) {
+        writeFileAsync('./Develop/db/db.json', JSON.stringify(notes))
+        res.json(note);
+    })
+});
+
